3a38b457709f8b8a02b38c1b60bba2a0584ec8f9
Alex Efros <powerman@powerman.name>
2020-09-17 13:03:06 +0200

    docs: update description in swagger (#32)

--- api/openapi/restapi/op/high_load_cup2020_api.go
+++ api/openapi/restapi/op/high_load_cup2020_api.go
@@ -64,6 +64,8 @@ func NewHighLoadCup2020API(spec *loads.Document) *HighLoadCup2020API {
 }
 
 /*HighLoadCup2020API # IT RALLY 2020 HighLoad Cup
+## List of all custom errors
+First number is HTTP Status code, second is value of "code" field in returned JSON object, text description may or may not match "message" field in returned JSON object.
 - 422.1000: wrong coordinates
 - 422.1001: wrong depth
 - 409.1002: no more active licenses allowed
