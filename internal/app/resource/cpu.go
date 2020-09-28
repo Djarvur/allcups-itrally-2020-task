64ea9c2316f910c73e1fdbf4e49f5681edbb1723
Alex Efros <powerman@powerman.name>
2020-09-28 15:20:32 +0300

    happy linter

--- internal/app/resource/cpu.go
+++ internal/app/resource/cpu.go
@@ -1,3 +1,5 @@
+// Package resource implements virtual resources, which simulate some
+// real-world resources without actually consuming them.
 package resource
 
 import (
@@ -8,11 +10,14 @@ import (
 
 type Ctx = context.Context
 
+// CPU is a resource which can be consumed for up to time.Second per
+// real-time second (i.e. it's a single-core CPU).
 type CPU struct {
 	freq time.Duration
 	tick chan struct{}
 }
 
+// NewCPU creates and returns new CPU.
 func NewCPU(hz int) *CPU {
 	const compensate = 2 * time.Millisecond // Compensate slow consumer for up to 0.002s.
 	if hz < 1 {
@@ -24,14 +29,20 @@ func NewCPU(hz int) *CPU {
 	}
 }
 
+// Provide should be started in background once per each CPU instance.
+// It'll return nil when ctx is done.
+//
+// When Provide is not running Consume won't be able to consume resources
+// and will return only when their ctx is done.
 func (c *CPU) Provide(ctx Ctx) error {
 	prev := time.Now().Round(c.freq)
-	tickc := time.Tick(time.Millisecond)
+	ticker := time.NewTicker(time.Millisecond)
+	defer ticker.Stop()
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
-		case now := <-tickc:
+		case now := <-ticker.C:
 			now = now.Round(c.freq)
 			for i := 0; i < int(now.Sub(prev)/c.freq); i++ {
 				select {
@@ -44,6 +55,9 @@ func (c *CPU) Provide(ctx Ctx) error {
 	}
 }
 
+// Consume t resources of this CPU instance.
+// It returns nil if consumed successfully or ctx.Err() if ctx is done
+// earlier than t resources will be consumed.
 func (c *CPU) Consume(ctx Ctx, t time.Duration) error {
 	for ticks := int(t.Round(c.freq) / c.freq); ticks > 0; ticks-- {
 		select {
